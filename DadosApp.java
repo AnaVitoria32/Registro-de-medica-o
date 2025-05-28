589680dad1d83f59ccdf078bfafce6a862c89b00
Ana Vitória Menezes de Paula <135264780+AnaVitoria32@users.noreply.github.com>
2025-05-28 11:22:41 -0300

    Add files via upload

---
+++ DadosApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication32;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alunolages
+ */
+public class DadosApp {
+    public static List<Medicamento> listaTodosMedicamentos= new ArrayList<>();
+}
